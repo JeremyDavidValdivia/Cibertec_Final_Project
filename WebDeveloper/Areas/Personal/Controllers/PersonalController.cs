b0241e764f6afef27b7a788a03e0195fd1abd3e1
JeremyDavidValdivia <jeremy.david@jcservicios.com>
2016-07-25 19:50:58 -0500

    Aplicacion incluyendo el EDIT.

--- WebDeveloper/Areas/Personal/Controllers/PersonalController.cs
+++ WebDeveloper/Areas/Personal/Controllers/PersonalController.cs
@@ -1,49 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using WebDeveloper.DataAccess;
 using WebDeveloper.Model;
-using WebDeveloper.Models;
+
 
 namespace WebDeveloper.Areas.Personal.Controllers
 {
     [Authorize]
     public class PersonalController : Controller
     {
-        private readonly PersonRepository _personalRepository;
-        public PersonalController(PersonRepository personalRepository)
+        private readonly PersonRepository _personRepository;
+        public PersonalController(PersonRepository personRepository)
         {
-            _personalRepository = personalRepository;
+            _personRepository = personRepository;
         }
-
+        [OutputCache(Duration = 0)]
         public ActionResult Index()
         {
-            return View(_personalRepository.GetListDto());
+            return View(_personRepository.GetListDto());
         }
 
         public PartialViewResult EmailList(int? id)
         {
-            if(!id.HasValue) return null;
-            return PartialView("_EmailList",_personalRepository.EmailList(id.Value));
+            if (!id.HasValue) return null;
+            return PartialView("_EmailList", _personRepository.EmailList(id.Value));
         }
 
-        public ActionResult Create()
+        public PartialViewResult Create()
         {
             return PartialView("_Create");
         }
 
-        //public PartialViewResult Create()
-        //{
-        //    return PartialView("_Create");
-        //}
-
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create(Person person)
-        {
+        {            
             if (!ModelState.IsValid) return PartialView("_Create", person);
             person.rowguid = Guid.NewGuid();
             person.BusinessEntity = new BusinessEntity
@@ -51,8 +47,26 @@ namespace WebDeveloper.Areas.Personal.Controllers
                 rowguid = person.rowguid,
                 ModifiedDate = person.ModifiedDate
             };
-            _personalRepository.Add(person);
-            return RedirectToAction("Index");
+            _personRepository.Add(person);            
+            return new HttpStatusCodeResult(HttpStatusCode.OK); //RedirectToAction("Index");
+        }
+
+        [OutputCache(Duration =0)]
+        public ActionResult Edit(int id)
+        {
+            var person = _personRepository.GetById(id);
+            if (person == null) return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            return PartialView("_Edit", person);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [OutputCache(Duration = 0)]
+        public ActionResult Edit(Person person)
+        {
+            if (!ModelState.IsValid) return PartialView("_Edit", person);
+            _personRepository.Update(person);
+            return RedirectToRoute("Personal_default");
         }
 
     }
